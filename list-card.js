d14715d6aa029ae400cb20a162cbfd94cbe62d73
Ixitxachitl <kramerfm@gmail.com>
2025-08-19 18:35:33 -0700

    Update list-card.js

--- list-card.js
+++ list-card.js
@@ -1,4 +1,4 @@
-console.log(`%clist-card\n%cVersion: ${'0.4.2'}`, 'color: rebeccapurple; font-weight: bold;', '');
+console.log(`%clist-card\n%cVersion: ${'0.4.3'}`, 'color: rebeccapurple; font-weight: bold;', '');
 
 /* =========================
    List Card (runtime)
